fbe2791aedd94945faf2d2e3a3c1b2e07236fa7f
Jason Hettmansperger <jjbeanz@Jasons-MacBook-Pro-8.local>
2016-02-07 16:22:00 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var pkg = require('./package.json')
+var provider = require('koop-provider')
+
+var dcmetro = provider({
+  name: 'dcmetro',
+  version: pkg.version,
+  model: require('./model'),
+  controller: require('./controller'),
+  routes: require('./routes')
+})
+
+module.exports = dcmetro
